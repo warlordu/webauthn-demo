91ee5cb3beac05f5e6511d410f58177a388db12a
warlordu <97518343+warlordu@users.noreply.github.com>
2022-06-29 12:02:23 +0530

    Update app.js
    Changing code to support heroku

--- app.js
+++ app.js
@@ -32,8 +32,10 @@ app.use('/', defaultroutes)
 app.use('/password', passwordauth)
 app.use('/webauthn', webuathnauth)
 
-const port = config.port || 3000;
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 console.log(`Started app on port ${port}`);
 
 module.exports = app;
